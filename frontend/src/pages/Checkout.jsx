b35940163ad57992a94480308bc5e2594326eb6e
Kenn <hojikenn1@gmail.com>
2025-10-06 23:33:09 +0800

    Update

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -184,6 +184,26 @@ const Checkout = () => {
             </div>
 
             {/* Right side */}
+            <div className="bg-gray-50 p-6 rounded-lg">
+                <h3 className="text-lg mb-4">Order Summary</h3>
+                <div className="border-t py-4 mb-4">
+                    {
+                        cart.products.map((product, index) => (
+                            <div key={index} className='flex items-start justify-between py-2 border-b'>
+                                <div className="flex items-start">
+                                    <img src={product.image} alt={product.name} className='w-20 h-24 object-cover mr-4'/>
+                                    <div>
+                                        <h3 className='text-md'>{product.name}</h3>
+                                        <p className='text-gray-500'>Size: {product.size}</p>
+                                        <p className='text-gray-500'>Color: {product.color}</p>
+                                    </div>
+                                </div>
+                                <p className='text-xl'>${product.price?.toLocaleString()}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
